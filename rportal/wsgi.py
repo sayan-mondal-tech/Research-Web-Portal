2483830822bfb3d8b24b3cdd53d3bc20b9ccd993
Monsij Biswal <biswalmonsij@gmail.com>
2019-07-31 01:29:57 +0530

    Fix app

--- rportal/wsgi.py
+++ rportal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for portalsite project.
+WSGI config for rportal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portalsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rportal.settings")
 
 application = get_wsgi_application()
-
