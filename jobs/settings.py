5a2a2c52f70ec84c0bba7744d498ef365b2417a7
Monsij Biswal <biswalmonsij@gmail.com>
2019-11-16 19:09:23 +0530

    Remove social_core

--- jobs/settings.py
+++ jobs/settings.py
@@ -71,10 +71,6 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.github.GithubOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.facebook.FacebookOAuth2',
-
     'django.contrib.auth.backends.ModelBackend',
 )
 
